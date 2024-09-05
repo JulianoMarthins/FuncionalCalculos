9ba14e476f1648b1e27b2a9dcb66780244a9a05d
Juliano Martins de Souza <julianopoamartins@gmail.com>
2024-09-05 16:12:55 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncionalCalculos.Entities {
+    internal class Product {
+
+        public string Nome { get; set;}
+        public double Price { get; set;}
+
+
+
+        public Product(string nome, double price) {
+            Nome = nome;
+            Price = price;
+        }
+
+
+        public override string ToString() {
+            string valorFormatado = string.Format(CultureInfo.GetCultureInfo("pt-br"), "{0:C}", Price);
+            return $"{Nome} R${valorFormatado}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FuncionalCalculos.Entities;
+
+namespace FuncionalCalculos {
+     internal class Program {
+          static void Main(string[] args) {
+
+               Console.WriteLine("Programado pro JulianoMartins");
+
+               List<Product> produto = new List<Product>();
+
+               produto.Add(new Product("Playstation 5", 3800));
+               produto.Add(new Product("Xbox Serie X", 4000));
+               produto.Add(new Product("Televisor LG", 2300));
+               produto.Add(new Product("Home Theater LG", 1600));
+
+
+               double total = 0;
+               Console.WriteLine("\nLista de compras.");
+               foreach (Product x in produto) {
+                    total += x.Price;
+                    Console.WriteLine(x.ToString());
+               }
+
+               string valorFormatado = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", total);
+
+               Console.WriteLine($"\nTotal da compra:\nR$ {valorFormatado}");
+
+               produto.RemoveAll(p => p.Price >= 2000);
+
+               Console.WriteLine("\nProdutos com valores abaixo de R$ 2.000,00");
+               foreach(Product p in produto) {
+                    Console.WriteLine(p.ToString());
+               }
+
+          }
+     }
+}
